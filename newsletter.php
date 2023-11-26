44914959cdd81bd1614b081d48ceb6d44eec054b
ebbuckson <114437947+ebbuckson@users.noreply.github.com>
2023-11-26 10:00:12 -0500

    Add files via upload

---
+++ newsletter.php
@@ -0,0 +1,45 @@
+<?php
+define('DB_HOST', 'localhost'); 
+define('DB_USER', 'root');
+define('DB_PASS', ''); 
+define('DB_NAME', 'dataform');
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if($conn->connect_error) {
+    die('Connection Failed ' . $conn->connect_error);
+    }
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {     
+$email = $_POST["Email"];
+if ( empty($email) ) {
+    // At least one variable is empty
+    echo "Please fill in all the required fields.";
+    return;
+}
+$query = $conn->prepare("INSERT INTO newsletter (Email) 
+VALUES (?)");
+$query->bind_param('s', $email);
+$query->execute();
+$conn->close();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Form</title>
+    <script>
+    </script>
+    </head>
+    <body>
+    <!--we will start here-->
+        <p> Thanks for subscribing! </p> <break>
+
+        <a href="https://ebbuckson.github.io/EdensEmbrace/">Click here to return to Home</a>
+    </body>
+
+
+</html>
